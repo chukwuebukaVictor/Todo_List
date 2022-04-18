50c9005ff5759a5aef1613c3fa676ddbc9d431f2
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-04-18 08:23:21 +0100

    Setup for linkedin profile

--- src/modules/add.js
+++ src/modules/add.js
@@ -2,7 +2,7 @@ import addToList from './addToUI.js';
 import ToDo from './todo.js';
 
 function add(e) {
-  if (e.code === 'Enter') {
+  if (e.key === 'Enter') {
     const newItem = new ToDo(this.value, false);
     localStorage.setItem('todoList', JSON.stringify(newItem.getList()));
     this.value = '';
